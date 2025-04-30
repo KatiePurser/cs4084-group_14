f8f1ec71f9684e3159e3c205cb72d5e268c8eb17
Sohaila Awaga <131364738+sohailaawaga@users.noreply.github.com>
2025-04-30 15:05:12 +0100

    Revert "Merge branch 'main' into Sohaila"
    This reverts commit 5340885b77c809ac63350c229d6c6a969b5de444, reversing
    changes made to 90138b3c05dfcd1dea9700e764dd371914916cd7.

--- app/src/main/java/com/example/fashionfriend/home/MainActivity.java
+++ app/src/main/java/com/example/fashionfriend/home/MainActivity.java
@@ -36,8 +36,6 @@ import com.example.fashionfriend.data.database.Reminder;
 import com.example.fashionfriend.outfitCreation.CreateOutfitActivity;
 import com.example.fashionfriend.viewAndEditOutfit.ViewAndEditOutfitActivity;
 
-import com.example.fashionfriend.wardrobe.WardrobeActivity;
-
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -208,7 +206,6 @@ public class MainActivity extends BaseActivity {
                     startActivity(new Intent(MainActivity.this, AddClothingItemActivity.class));
                 } else if (id == R.id.wardrobe_button) {
                     // Navigate to wardrobe view (implementation pending)
-                    startActivity(new Intent(MainActivity.this, WardrobeActivity.class));
                 } else if (id == R.id.outfits_button) {
                       // Navigates to outfits view
                       Intent intent = new Intent(MainActivity.this, OutfitList.class);
